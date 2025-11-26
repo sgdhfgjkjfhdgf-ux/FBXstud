5ac7ab9b452dc33a6ba5f36f9f374371c18c31a5
Александр  Лазарев <scorpocha@MacBook-m-competition-RS-AMG-GTI-CS-VRS-2JZ-GTE.local>
2025-11-26 16:27:33 +0300

    Slow down marquee animation

--- src/components/FBXLanding.tsx
+++ src/components/FBXLanding.tsx
@@ -284,7 +284,7 @@ export function FBXLanding() {
           100% { transform: translateX(-50%); }
         }
         .animate-marquee {
-          animation: marquee 20s linear infinite;
+          animation: marquee 40s linear infinite;
         }
       `}} />
     </div>
